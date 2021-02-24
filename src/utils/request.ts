7d30ca34c222f8b98f1521c9c011f3bf1a592284
Starrah <46821571+starrah@users.noreply.github.com>
2021-02-25 01:39:09 +0800

    修复图片请求URL的bug

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -17,5 +17,5 @@ export function convertResUrl(url?: string) {
     if (!url) return ""
     if (url.substr(0, 8) === "https://") url = url.substr(8)
     else if (url.substr(0, 7) === "http://") url = url.substr(7)
-    return axios.defaults.baseURL + "/static/" + url
+    return (axios.defaults.baseURL?axios.defaults.baseURL:"") + "/static/" + url
 }
